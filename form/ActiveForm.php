748f7916563f5899197139cbcc47126ca71d5102
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-10-20 15:22:53 +0400

    fix ActiveForm

--- form/ActiveForm.php
+++ form/ActiveForm.php
@@ -8,4 +8,11 @@ use yii\widgets\ActiveForm as YiiActiveForm;
 class ActiveForm extends YiiActiveForm
 {
 
+    public function field($model, $attribute, $options = [])
+    {
+        if (!array_key_exists('class', $options)) {
+            //$options['class'] = '';
+        }
+        return parent::field($model, $attribute, $options);
+    }
 }
