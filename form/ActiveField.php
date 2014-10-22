afdbebd532b671501be097fa9639bbd4ef96e14d
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-10-22 23:57:40 +0400

    fix readOnly 2

--- form/ActiveField.php
+++ form/ActiveField.php
@@ -10,11 +10,29 @@ class ActiveField extends YiiActiveField
 
     public $readOnly = false;
 
+    public $radioOptions = [];
+
+    public $checkboxOptions = [];
+
     public function init()
     {
-        parent::init();
         if ($this->readOnly) {
             $this->inputOptions['readonly'] = true;
+            $this->radioOptions['disabled'] = true;
+            $this->checkboxOptions['disabled'] = true;
         }
+        parent::init();
+    }
+
+    public function radio($options = [], $enclosedByLabel = true)
+    {
+        $options = array_merge($this->radioOptions, $options);
+        return parent::radio($options, $enclosedByLabel);
+    }
+
+    public function checkbox($options = [], $enclosedByLabel = true)
+    {
+        $options = array_merge($this->checkboxOptions, $options);
+        return parent::checkbox($options, $enclosedByLabel);
     }
 }
