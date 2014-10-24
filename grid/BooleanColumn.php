601dbd3814beebbd7e626d2f58e0b96a059ea93d
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-10-24 18:08:09 +0400

    fix BooleanColumn

--- grid/BooleanColumn.php
+++ grid/BooleanColumn.php
@@ -2,8 +2,18 @@
 
 namespace yii\mozayka\grid;
 
+use Yii;
+
 
 class BooleanColumn extends DataColumn
 {
 
+    protected function renderDataCellContent($model, $key, $index)
+    {
+        if (is_null($this->content)) {
+            return Yii::t('mozayka', $model->{$this->attribute} ? 'Yes' : 'No');
+        } else {
+            return parent::renderDataCellContent($model, $key, $index);
+        }
+    }
 }
