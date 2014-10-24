cce66711aa0561eb90dfe8192de309acd4e33454
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-10-24 22:30:54 +0400

    fix BooleanColumn

--- grid/BooleanColumn.php
+++ grid/BooleanColumn.php
@@ -11,7 +11,11 @@ class BooleanColumn extends DataColumn
     protected function renderDataCellContent($model, $key, $index)
     {
         if (is_null($this->content)) {
-            return Yii::t('mozayka', $model->{$this->attribute} ? 'Yes' : 'No');
+            if ($model->{$this->attribute}) {
+                return '<span class="label label-success">' . Yii::t('mozayka', 'Yes') . '</span>';
+            } else {
+                return '<span class="label label-danger">' . Yii::t('mozayka', 'No') . '</span>';
+            }
         } else {
             return parent::renderDataCellContent($model, $key, $index);
         }
