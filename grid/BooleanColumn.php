ac3f97e234187f4c724198b8866209b6e7bfe806
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-11-02 23:50:52 +0300

    fix BooleanColumn

--- grid/BooleanColumn.php
+++ grid/BooleanColumn.php
@@ -8,15 +8,16 @@ use Yii;
 class BooleanColumn extends DataColumn
 {
 
-    public $format = 'html';
-
-    public function getDataCellValue($model, $key, $index)
+    protected function renderDataCellContent($model, $key, $index)
     {
-        $value = parent::getDataCellValue($model, $key, $index);
-        if ($value) {
-            return '<span class="label label-success">' . Yii::t('mozayka', 'Yes') . '</span>';
-        } else {
-            return '<span class="label label-danger">' . Yii::t('mozayka', 'No') . '</span>';
+        $value = $this->getDataCellValue($model, $key, $index);
+        if (is_int($value)) {
+            if ($value) {
+                return '<span class="label label-success">' . Yii::t('mozayka', 'Yes') . '</span>';
+            } else {
+                return '<span class="label label-danger">' . Yii::t('mozayka', 'No') . '</span>';
+            }
         }
+        return parent::renderDataCellContent($model, $key, $index);
     }
 }
