97346cd50dbe17658fe702d934e6e9b1ff28417b
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-11-19 23:29:41 +0300

    fix layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -4,8 +4,10 @@ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use yii\mozayka\MozaykaAsset;
-/* @var $this \yii\web\View */
-/* @var $content string */
+/**
+ * @var yii\web\View $this
+ * @var string $content
+ */
 
 MozaykaAsset::register($this);
 $this->beginPage();
@@ -13,11 +15,11 @@ $this->beginPage();
 <!DOCTYPE html>
 <html lang="<?php echo Yii::$app->language; ?>">
 <head>
-<meta charset="<?php echo Yii::$app->charset; ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<?php echo Html::csrfMetaTags(); ?>
-<title><?php echo Html::encode($this->title); ?></title>
-<?php $this->head(); ?>
+    <meta charset="<?php echo Yii::$app->charset; ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php echo Html::csrfMetaTags(); ?>
+    <title><?php echo Html::encode($this->title); ?></title>
+    <?php $this->head(); ?>
 </head>
 <body>
 
@@ -57,8 +59,8 @@ $this->beginPage();
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-left">&copy; My Company <?php echo date('Y'); ?></p>
+        <p class="pull-right"><?php echo Yii::powered(); ?></p>
     </div>
 </footer>
 
