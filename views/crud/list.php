6cd1f21ac22e819c3668d02c6c1445c5d44aaa81
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-11-22 22:52:23 +0300

    fix css

--- views/crud/list.php
+++ views/crud/list.php
@@ -55,4 +55,25 @@ echo Html::tag('div', ButtonGroup::widget([
 $formClass::end();
 echo Html::endTag('div');*/
 
+
+echo Html::beginTag('div', ['class' => 'panel panel-default']);
+echo Html::tag('div', Html::tag('h3', $this->title, ['class' => 'panel-title']), ['class' => 'panel-heading']);
+//$form = $formClass::begin($formConfig);
+
+echo Html::tag('div', /*$form->fields($model, $fields)*/'qwe', ['class' => 'panel-body']);
+
+$buttons = [];
+$buttons[] = Html::submitButton(Yii::t('mozayka', 'Save'), ['class' => 'btn btn-primary']);
+/*if ($canList) {
+    $buttons[] = Html::a(Yii::t('mozayka', 'Back'), ['list'], ['class' => 'btn btn-default']);
+}*/
+
+/*echo Html::tag('div', ButtonGroup::widget([
+    'buttons' => $buttons,
+    'options' => ['class' => 'pull-right']
+]), ['class' => 'panel-footer clearfix hidden-print']);*/
+
 echo $gridClass::widget($gridConfig);
+
+//$formClass::end();
+echo Html::endTag('div');
