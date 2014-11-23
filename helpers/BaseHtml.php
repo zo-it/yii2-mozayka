5ee8a0ccaf4b9de416e9c77931242c038f436762
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-11-23 14:43:17 +0300

    add helpers/Html.php, helpers/BaseHtml.php

---
+++ helpers/BaseHtml.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace yii\mozayka\helpers;
+
+use yii\kladovka\helpers\BaseHtml as KladovkaBaseHtml;
+
+
+class BaseHtml extends KladovkaBaseHtml
+{
+}
--- helpers/ModelHelper.php
+++ helpers/ModelHelper.php
@@ -6,3 +6,7 @@ namespace yii\mozayka\helpers;
 class ModelHelper extends BaseModelHelper
 {
 }
+
+/*if (!class_exists('yii\helpers\ModelHelper', false)) {
+    class_alias('yii\mozayka\helpers\ModelHelper', 'yii\helpers\ModelHelper', false);
+}*/
