b476fc27f50c592fb69f12de91b4efedef868fce
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-11-23 23:45:37 +0300

    fix ActionColumn

--- grid/ActionColumn.php
+++ grid/ActionColumn.php
@@ -3,7 +3,10 @@
 namespace yii\mozayka\grid;
 
 use yii\grid\ActionColumn as YiiActionColumn,
-    yii\mozayka\helpers\ModelHelper;
+    yii\mozayka\helpers\ModelHelper,
+    yii\helpers\Html,
+    yii\mozayka\web\DropdownAsset,
+    Yii;
 
 
 class ActionColumn extends YiiActionColumn
@@ -39,5 +42,25 @@ class ActionColumn extends YiiActionColumn
             '~\s+data\-method\="[^"]*"~i' => ''
         ];
         return preg_replace(array_keys($fix), array_values($fix), parent::renderDataCellContent($model, $key, $index));
+    }
+
+    public function renderDataCell($model, $key, $index)
+    {
+        $cellContent = $this->renderDataCellContent($model, $key, $index);
+        if ($cellContent != $this->grid->emptyCell) {
+            $content = Html::button(Yii::t('mozayka', 'Actions') . ' <span class="caret"></span>', [
+                'id' => 'actions-trigger',
+                'class' => 'btn btn-default btn-xs',
+                'data-dropdown2' => '#actions-dropdown2'
+            ]) . Html::tag('div', Html::tag('div', $cellContent, ['class' => 'dropdown2-panel']), [
+                'id' => 'actions-dropdown2',
+                'class' => 'dropdown2 dropdown2-tip dropdown2-anchor-right'
+            ]);
+            if (!Yii::$app->getRequest()->getIsAjax()) {
+                DropdownAsset::register($this->grid->getView());
+            }
+            return Html::tag('td', $content, $this->contentOptions);
+        }
+        return parent::renderDataCell($model, $key, $index);
     }
 }
