3c5d2f12d5b28bce67d81213689bbc208d048304
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-11-25 01:16:24 +0300

    fix textarea

--- form/TextField.php
+++ form/TextField.php
@@ -8,6 +8,9 @@ class TextField extends ActiveField
 
     public function init()
     {
+        if (!array_key_exists('style', $this->inputOptions)) {
+            $this->inputOptions['style'] = 'height: 100px;';
+        }
         parent::init();
         $this->textarea();
     }
