c3aea99920772085c31be6e65e8326b3f04edecd
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-12-09 23:22:31 +0300

    fix js

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,20 @@
 jQuery(document).on('contextmenu', '.grid-view tbody > tr > td', function (event) {
-    var $dropdown = jQuery(this).siblings('.actions').find('.dropdown2');
-    if ($dropdown) {
+    var dropdown = jQuery(this).siblings('.actions').find('.dropdown2');
+    if (dropdown) {
         event.preventDefault();
         event.stopPropagation();
-        jQuery('<div></div>').css({
+        jQuery(document).dropdown2('hide');
+        jQuery('<div id="contextmenu-trigger"></div>').css({
             'position': 'absolute',
             'width': 30,
             'height': 1
         }).offset({
             'left': event.pageX - 15,
             'top': event.pageY
-        }).prependTo(this).attr('data-dropdown2', '#' + $dropdown.attr('id')).dropdown2('show').remove();
+        }).prependTo(this).attr('data-dropdown2', '#' + dropdown.attr('id')).dropdown2('show');
     }
+});
+
+jQuery(document).on('hide', '.dropdown2', function (event) {
+    jQuery('#contextmenu-trigger').remove();
 });
